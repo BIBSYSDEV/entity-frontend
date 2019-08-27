d1ca0dad52ebc1b9d390cad2b1ac7fa20625e608
en <eirik.nilsen@unit.no>
2019-08-27 13:33:42 +0200

    AUTREG-231: Real test data

--- src/Search.tsx
+++ src/Search.tsx
@@ -12,32 +12,6 @@ export interface SearchProps {
     entityId: string;
 }
 
-
-
-let testData: object[] = [
-    {id: "HUME00001", preferredLabel: [{ lang:"NB", value: "Humaniora"}], dato: "1994-03-21"},
-    {id: "HUME00002", related: "HUME00001", preferredLabel: [{ lang:"NB", value: "Humanistiske fag"}], dato: "2009-10-22"},
-    {id: "HUME00003", related: "HUME00001", preferredLabel: [{ lang:"NB", value: "Humanvitenskap"}], dato: "1994-03-21"},
-    {id: "HUME00004", related: "HUME00001", preferredLabel: [{ lang:"NB", value: "Humord"}], dato: "1994-03-21"},
-    {id: "HUME00005", preferredLabel: [{ lang:"NB", value: "Arkeologi"}], dato: "1995-03-01", definition: "Kulturen, livet og levemåten i eldre tider grunnet på utgravninger og funn i jorda <SNL>", broader: "HUME00001", broader1: "HUME00005", seeAlso: "HUME12563"},
-    {id: "HUME00006", preferredLabel: [{ lang:"NB", value: "Fortidsminner"}], dato: "2009-10-22", broader: "HUME00005", broader1: "HUME00005", seeAlso: ["HUME04581", "HUME04546", "HUME12105"]},
-    {id: "HUME00007", related: "HUME00006", preferredLabel: [{ lang:"NB", value: "Fornminner"}], dato: "2009-10-22"},
-    {id: "HUME00008", preferredLabel: [{ lang:"NB", value: "Faste fortidsminner"}], dato: "2015-11-20", broader: "HUME00006", broader1: "HUME00005"},
-    {id: "HUME00009", preferredLabel: [{ lang:"NB", value: "Boplasser"}], dato: "2009-10-22", broader: "HUME00008", broader1: "HUME00005", seeAlso: ["HUME09034", "HUME09054", "HUME09052"]},
-    {id: "HUME00010", preferredLabel: [{ lang:"NB", value: "Crannog"}], dato: "2006-11-24", definition: "Befestet boplass anlagt i innsjø i Irland, Skottland og England <UBO>", broader: "HUME00009", broader1: "HUME00005", seeAlso: "HUME05291"},
-    {id: "HUME00011", preferredLabel: [{ lang:"NB", value: "Hellere"}], dato: "2009-10-22", definition: "Overheng under bergvegg eller liten hule hvor åpningen er større enn dybden. Heller har i stor grad vært brukt som boplass og som leger under fangst og fiske <UBO>", broader: "HUME00009", broader1: "HUME00005"},
-    {id: "HUME00012", preferredLabel: [{ lang:"NB", value: "Bygdeborger"}], dato: "2009-10-22", broader: "HUME00008", broader1: "HUME00005", seeAlso: ["HUME05293", "HUME05291"]},
-    {id: "HUME00013", preferredLabel: [{ lang:"NB", value: "Dyregraver"}], dato: "2009-10-22", broader: "HUME00008", broader1: "HUME00005"},
-    {id: "HUME00014", preferredLabel: [{ lang:"NB", value: "Gravplasser"}], dato: "2009-10-22", alternativeLabel: "Arkeologi", broader: "HUME00008", broader1: "HUME00005", seeAlso: "HUME08415"},
-    {id: "HUME00015", related: "HUME00014", preferredLabel: [{ lang:"NB", value: "Gravanlegg"}], dato: "1994-03-21"},
-    {id: "HUME00016", related: "HUME00014", preferredLabel: [{ lang:"NB", value: "Gravfelt"}], dato: "1994-03-21"},
-    {id: "HUME00017", related: "HUME00014", preferredLabel: [{ lang:"NB", value: "Nekropoler"}], dato: "2009-10-22"},
-    {id: "HUME00018", preferredLabel: [{ lang:"NB", value: "Graver"}], dato: "2009-10-22", broader: "HUME00014", broader1: "HUME00005"},
-    {id: "HUME00019", preferredLabel: [{ lang:"NB", value: "Båtgraver"}], dato: "2009-10-22", broader: "HUME00018", broader1: "HUME00005"},
-    {id: "HUME00020", preferredLabel: [{ lang:"NB", value: "Gravhauger"}], dato: "2009-10-22", broader: "HUME00014", broader1: "HUME00005"},
-    {id: "HUME00021", preferredLabel: [{ lang:"NB", value: "Gårdshauger"}], dato: "2011-09-14", broader: "HUME00020", broader1: "HUME00005"}
-]
-
 const EMPTY_SEARCH_RESULTS: ResultType[] = new Array<ResultType>(); 
 
 const Search = (props: SearchProps): any => {
@@ -51,8 +25,8 @@ const Search = (props: SearchProps): any => {
     const findSingleEntity = (entityId: string): ResultType[] => {
         let result: ResultType[] = [];
         for (let index = 0; index < testSearchResult.hits.hit.length; index++) {
-            if ((testData[index] as ResultType).id === entityId) {
-                result.push(testData[index] as ResultType);
+            if ((testResult[index] as ResultType).id === entityId) {
+                result.push(testResult[index] as ResultType);
                 break;
             }
         }
@@ -93,6 +67,5 @@ const Search = (props: SearchProps): any => {
         </div>
     );
 }
-
 
 export default Search;
