5036123715524fd751f9b94b34deabc2849e379a
en <eirik.nilsen@unit.no>
2019-09-02 11:59:54 +0200

    AUTREG-231: Linting

--- src/ResultPresentation.tsx
+++ src/ResultPresentation.tsx
@@ -82,9 +82,9 @@ const ResultPresentation = (props: ResultProps): any => {
                 (value as any[]).map((element: string) => <Typography><Link href={processLink(element)}>{element}</Link></Typography>) 
                 :
                 value === Object(value) ?
-                <Typography><Link href={"/".concat(registryName, "/Search/", value['value'])}>{value['valule']}</Link></Typography>
-                :
-                <Typography><Link href={"/".concat(registryName, "/Search/", value)}>{value}</Link></Typography>
+                    <Typography><Link href={"/".concat(registryName, "/Search/", value['value'])}>{value['valule']}</Link></Typography>
+                    :
+                    <Typography><Link href={"/".concat(registryName, "/Search/", value)}>{value}</Link></Typography>
             }
         </Typography>;
     };
