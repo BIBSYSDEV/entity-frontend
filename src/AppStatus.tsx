792156c38b28386dc289185253f7d19632e4037c
Sarah Serussi <sarah.serussi@acando.no>
2019-09-04 14:16:06 +0200

    Add interface to AppStatus component

--- src/AppStatus.tsx
+++ src/AppStatus.tsx
@@ -1,12 +1,20 @@
 import React from 'react';
 import './AppStatus.css';
 
-const AppStatus = ({status}: {status: string}): any => {
-    return (<div style={{
-        fontSize: '20px',
-        color: '#aaaaaa',
-        padding: '0.5em'
-    }}>{status}</div>)
+interface AppStatusProps {
+	status: string;
 }
+
+const AppStatus: React.FC<AppStatusProps> = ({ status }) => (
+	<div
+		style={{
+			fontSize: '20px',
+			color: '#aaaaaa',
+			padding: '0.5em'
+		}}
+	>
+		{status}
+	</div>
+);
 
 export default AppStatus;
