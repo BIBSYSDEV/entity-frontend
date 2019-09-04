ef27148565ae3cd780cf969ddc889ca423c2fed0
Sarah Serussi <sarah.serussi@acando.no>
2019-09-04 14:17:12 +0200

    Remove unnecessary use of useRef hook

--- src/RegistryList.tsx
+++ src/RegistryList.tsx
@@ -1,29 +1,31 @@
-import React, { useRef } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
-import Button from "@material-ui/core/Button";
+import Button from '@material-ui/core/Button';
 import { fetchApiKey } from './utils';
 
 export interface DataProps {
-    registries: string;
-    setApiKey(apiKey: string): void;
+	registries: string;
+	setApiKey(apiKey: string): void;
 }
 
-const RegistryList = (props: DataProps): any => {
+const RegistryList = (props: DataProps) => {
+	const { registries, setApiKey } = props;
+	const registryList: string[] = JSON.parse(registries);
 
-    const { registries, setApiKey } = props;
-
-    const renderListItems = (): object => {
-        return JSON.parse(registries).map((registry: string): object =>
-            <li key={registry}>
-                <Link to={registry} style={{textDecoration: 'none'}}><Button onClick={() => fetchApiKey(registry, setApiKey)}>{registry}</Button></Link>
-            </li>); 
-    };
-
-    const listItems = useRef(renderListItems());
-
-    return (
-        <ul>{listItems.current}</ul>
-    );
-}
+	return (
+		<ul>
+			{registryList.length > 0 &&
+				registryList.map(registry => (
+					<li key={registry}>
+						<Link to={registry} style={{ textDecoration: 'none' }}>
+							<Button onClick={() => fetchApiKey(registry, setApiKey)}>
+								{registry}
+							</Button>
+						</Link>
+					</li>
+				))}
+		</ul>
+	);
+};
 
 export default RegistryList;
