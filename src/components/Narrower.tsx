ed82a9f8d86832b60621cea4a6c800dab8381bd8
Sarah Serussi <sase@unit.no>
2019-09-11 15:04:52 +0200

    refactor narrower to display chip when it has data

--- src/components/Narrower.tsx
+++ src/components/Narrower.tsx
@@ -1,8 +1,10 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { withJsonFormsControlProps } from '@jsonforms/react';
 import AutoSuggest from './AutoSuggest';
 import { compose } from 'redux';
 import { withRouter, RouteComponentProps } from 'react-router-dom';
+import ChipLabel from './ChipLabel';
+import { Typography } from '@material-ui/core';
 
 interface NarrowerProps {
 	data: any;
@@ -18,14 +20,55 @@ const Narrower: React.FC<NarrowerProps & RouteComponentProps<any>> = ({
 }) => {
 	const registryName = match.params.registryName;
 
+	const [tempLabel, setTempLabel] = useState('');
+	const [id, setId] = useState('');
+	const [searchForLabel, setSearchForLabel] = useState(false);
+
+	useEffect(() => {
+		if (data) {
+			setId(data.toString());
+		} else {
+			setId('');
+		}
+	}, [data]);
+
+	useEffect(() => {
+		if (id) {
+			setSearchForLabel(true);
+		} else {
+			setSearchForLabel(false);
+		}
+	}, [id]);
+
+	const handleClick = (id: string) => {
+		setId(id);
+		setTempLabel('');
+		setSearchForLabel(true);
+		handleChange(path, id);
+	};
+
+	const handleDelete = (_: any) => {
+		setId('');
+		setSearchForLabel(false);
+		handleChange(path, '');
+	};
+
 	return (
 		<div style={{ paddingBottom: '1rem' }}>
-			<AutoSuggest
-				label="Narrower"
-				onChange={(value: string) => handleChange(path, value)}
-				registryName={registryName}
-				value={data}
-			/>
+			{searchForLabel ? (
+				<React.Fragment>
+					<Typography>Narrower</Typography>
+					<ChipLabel id={id} onDelete={handleDelete} />
+				</React.Fragment>
+			) : (
+				<AutoSuggest
+					label="Narrower"
+					onChange={(value: string) => setTempLabel(value)}
+					onClick={handleClick}
+					registryName={registryName}
+					value={tempLabel}
+				/>
+			)}
 		</div>
 	);
 };
